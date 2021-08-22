3e4868a214fb2bfcc2182aa2387185a3de7942fa
AKIFUMI <awakewalker007@example.com>
2021-08-22 16:15:04 +0700

    Implement gtag script

--- components/util/seo.tsx
+++ components/util/seo.tsx
@@ -51,6 +51,22 @@ const SEO: VFC<{
           }
         ]}
       />
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+          page_path: window.location.pathname,
+        });
+      `,
+        }}
+      />
     </>
   );
 };
